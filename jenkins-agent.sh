c78ed5ac426ab63b30dc7d549f25f55ce0f175ae
venkatdamarla <venkatdamarla@gmail.com>
2025-07-28 11:58:15 +0530

    CICD Practice

---
+++ jenkins-agent.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#resize disk from 20GB to 50GB
+growpart /dev/nvme0n1 4
+
+lvextend -L +10G /dev/mapper/RootVG-homeVol
+lvextend -L +10G /dev/mapper/RootVG-varVol
+lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
+
+xfs_growfs /home
+xfs_growfs /var/tmp
+xfs_growfs /var
+
+yum install java-17-openjdk -y
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+yum install zip -y
+
+# docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
