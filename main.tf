ab5cb615056a5bfd4f8fe4cefd3492342f080121
venkatdamarla <venkatdamarla@gmail.com>
2025-07-28 13:46:42 +0530

    CICD Practice

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "jenkins" {
   name = "jenkins"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-01bc7ebe005fb1cb2"] #replace your SG
-  subnet_id = "subnet-0ea509ad4cba242d7" #replace your Subnet
+  vpc_security_group_ids = ["sg-002a9cd825c319861"] #replace your SG
+  subnet_id = "subnet-074a3c9cdcc93cba6" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins.sh")
   tags = {
@@ -28,8 +28,8 @@ module "jenkins_agent" {
   name = "jenkins-agent"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-01bc7ebe005fb1cb2"] #replace your SG
-  subnet_id = "subnet-0ea509ad4cba242d7" #replace your Subnet
+  vpc_security_group_ids = ["sg-002a9cd825c319861"] #replace your SG
+  subnet_id = "subnet-074a3c9cdcc93cba6" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins-agent.sh")
   tags = {
